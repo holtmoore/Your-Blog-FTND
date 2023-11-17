669127a466eb843dc14b79f3183704a5fe042268
Holt Moore <holt.w.moore@gmail.com>
2023-11-17 00:57:01 -0600

    first commit

---
+++ src/components/Post.js
@@ -0,0 +1,17 @@
+export default function Post() {
+    return (
+        <div className="post">
+        <div className="image">
+        <img src="https://www.looper.com/img/gallery/my-hero-academia-the-reason-all-might-is-the-most-powerful-teacher/intro-1596067346.jpg" alt="All Might" />
+        </div>
+        <div className="texts">
+        <h2>My Hero Academia: The reason All Might is the most powerful teacher</h2>
+        <p className="info">
+        <a className="author">Holt Moore</a>
+        <time>2023-11-06 10:14</time>
+        </p>
+        <p className="summary">My Hero Academia is a series that has a lot of powerful characters, but All Might is the most powerful teacher in the series. Here's why.</p>
+        </div>
+      </div>
+    )
+}
