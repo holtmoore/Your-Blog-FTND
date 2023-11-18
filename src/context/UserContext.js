c14dff4ac3d25d28f26cd38bee480e9fda28753c
Holt Moore <holt.w.moore@gmail.com>
2023-11-17 18:11:45 -0600

    Login

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,11 +1,12 @@
-import {createContext} from "react";
+import {createContext, useState} from "react";
 
 export const UserContext = createContext({});
 
-export function UserProvider({children}) {
-    return (
-        <UserContext.Provider value={{}}>
-            {children}
-        </UserContext.Provider>
-    )
+export function UserContextProvider({children}) {
+  const [userInfo,setUserInfo] = useState({});
+  return (
+    <UserContext.Provider value={{userInfo,setUserInfo}}>
+      {children}
+    </UserContext.Provider>
+  );
 }
