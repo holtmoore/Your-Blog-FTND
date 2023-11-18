00a0285927530baddbe70ce56b94fe93a83959a0
Holt Moore <holt.w.moore@gmail.com>
2023-11-17 19:31:05 -0600

    login

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,47 +1,46 @@
-import React, {useContext, useEffect} from 'react';
-import { Link } from 'react-router-dom';
-import { UserContext } from '../context/UserContext';
-
+import {Link} from "react-router-dom";
+import {useContext, useEffect, useState} from "react";
+import {UserContext} from "../context/UserContext";
 
 export default function Header() {
-    const {setUserInfo,userInfo} = useContext(UserContext);
-    useEffect(() => {
-      fetch('http://localhost:4000/profile', {
-        credentials: 'include',
-      }).then(response => {
-        response.json().then(userInfo => {
-          setUserInfo(userInfo);
-        });
+  const {setUserInfo,userInfo} = useContext(UserContext);
+  useEffect(() => {
+    fetch('http://localhost:4000/profile', {
+      credentials: 'include',
+    }).then(response => {
+      response.json().then(userInfo => {
+        setUserInfo(userInfo);
       });
-    }, []);
+    });
+  }, []);
 
-    function logout() {
-        fetch('http://localhost:4000/logout', {
-          credentials: 'include',
-          method: 'POST',
-        });
-        setUserInfo(null);
-      }
-    
-      const username = userInfo?.username;
-    
-      return (
-        <header>
-          <Link to="/" className="logo">MyBlog</Link>
-          <nav>
-            {username && (
-              <>
-                <Link to="/create">Create new post</Link>
-                <a onClick={logout}>Logout ({username})</a>
-              </>
-            )}
-            {!username && (
-              <>
-                <Link to="/login">Login</Link>
-                <Link to="/register">Register</Link>
-              </>
-            )}
-          </nav>
-        </header>
-      );
-    }
+  function logout() {
+    fetch('http://localhost:4000/logout', {
+      credentials: 'include',
+      method: 'POST',
+    });
+    setUserInfo(null);
+  }
+
+  const username = userInfo?.username;
+
+  return (
+    <header>
+      <Link to="/" className="logo">MyBlog</Link>
+      <nav>
+        {username && (
+          <>
+            <Link to="/create">Create new post</Link>
+            <a onClick={logout}>Logout ({username})</a>
+          </>
+        )}
+        {!username && (
+          <>
+            <Link to="/login">Login</Link>
+            <Link to="/register">Register</Link>
+          </>
+        )}
+      </nav>
+    </header>
+  );
+}
